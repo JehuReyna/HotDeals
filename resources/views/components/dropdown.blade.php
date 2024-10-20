78a12acaf69e27175f51f32d5dc0c89c6be83d5e
JehuReyna <162845212+JehuReyna@users.noreply.github.com>
2024-10-19 18:18:03 -0600

    Avance mapa

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,75 @@
+<div class="dropdown">
+    <button class="dropbtn">Opciones</button>
+    <div class="dropdown-content">
+        <a href="#">
+            <div class="tooltip-container">
+                <span class="tooltip">Perfil</span>
+                <span class="text">
+                    <div class="borde-back">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"></path>
+                                <path fill-rule="evenodd"
+                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                </span>
+            </div>
+        </a> {{-- mostrará el perfil del usuario, solo se verá cuando tenga un perfil. --}}
+        <a href="#">
+            <div class="tooltip-container">
+                <span class="tooltip">Seguidos</span>
+                <span class="text">
+                    <div class="borde-back">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"></path>
+                                <path fill-rule="evenodd"
+                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                </span>
+            </div>
+        </a> {{-- mostrará a los vendedores que siguen y si se encuentran vendiendo --}}
+        <a href="#">
+            <div class="tooltip-container">
+                <span class="tooltip">acerca de nosotros</span>
+                <span class="text">
+                    <div class="borde-back">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"></path>
+                                <path fill-rule="evenodd"
+                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                </span>
+            </div>
+        </a>
+        <a href="#">
+            <span class="tooltip">terminos y condiciones</span>
+            <span class="text">
+                <div class="borde-back">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"></path>
+                            <path fill-rule="evenodd"
+                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+            </span>
+        </a>
+    </div>
+</div>
---
+++ resources/views/components/mapa.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styledropdown.css') }}">
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYmACEm27EsXqoSQwUekt2L0J9YP35AMw"></script>
+    <script>
+        function initMap() {
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 18,
+                center: {
+                    lat: 0,
+                    lng: 0
+                },
+                /* mapTypeId: 'terrain', // Modo relieve */
+                mapTypeControl: false,
+                scrollwheel: false, // Desactiva el zoom con la rueda del ratón
+                disableDoubleClickZoom: true, // Desactiva el zoom con doble clic
+                streetViewControl: false, // Desactiva el control de Street View
+                /* draggable: false, // Desactiva el arrastre del mapa */
+                zoomControl: false // Oculta los controles de zoom
+            });
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+
+                    var marker = new google.maps.Marker({
+                        position: pos,
+                        map: map
+                    });
+
+                    map.setCenter(pos);
+
+
+                    }, function() {
+                        handleLocationError(true, map.getCenter());
+                    }, {
+                        enableHighAccuracy: true, // Habilita alta precisión
+                        timeout: 2000, // Timeout en milisegundos
+                        maximumAge: 0 // No usar ubicación en caché
+                    }
+                );
+
+
+                navigator.geolocation.watchPosition(
+                    function(position) {
+                        var pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        var marker = new google.maps.Marker({
+                            position: pos,
+                            map: map
+                        });
+                        map.setCenter(pos);
+                    }, 
+                    function() {
+                        handleLocationError(true, map.getCenter());
+                    }, 
+                    {
+                        enableHighAccuracy: true, // Habilita alta precisión
+                        timeout: 2000, // Timeout en milisegundos
+                        maximumAge: 0 // No usar ubicación en caché
+                    }
+                );
+
+
+            } else {
+                handleLocationError(false, map.getCenter());
+            }
+        }
+
+        function handleLocationError(browserHasGeolocation, pos) {
+            console.log(browserHasGeolocation ? "Error: El servicio de geolocalización falló." :
+                "Error: Tu navegador no soporta geolocalización.");
+
+            var styles = [{
+                    "featureType": "poi",
+                    "elementType": "labels.text.fill",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "labels.text.stroke",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "labels.text.fill",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "labels.text.stroke",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "landscape",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "water",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                }
+            ];
+
+        }
+    </script>
+</head>
+{{-- 735px --}}
+
+<body onload="initMap()">
+    <div id="map" style="width:100%;height:100vh;"></div>
+</body>
+
+</html>
